3b9226a09c07b0c105316f43965036305790d0e9
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2024-03-20 15:35:53 -0700

    Update Functional
    After many hours of pain i got it to work

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const Discord = require("discord.js");
 var request = require("request");
-
+// sessons: figure out how to type in a string of numbers to communicate with the api
 const client = new Discord.Client({
   intents: [
     Discord.GatewayIntentBits.Guilds,
@@ -14,13 +14,17 @@ const client = new Discord.Client({
   ],
 });
 const channelID = "1218014373327540367";
-client.once("ready", () => {
+const guildId = "1218014373327540364";
+
+let botOnlin = false;
+client.on("ready", () => {
+  botOnlin = true;
+  console.log(`Logged in as ${client.user.tag}!`);
   const guild_ids = client.guilds.cache.map((guild) => guild.id);
-  prefix = "!"
-  
+  prefix = "!";
 
   const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
-
+  // while loop that checks if the bot is online
   console.log("Bot is online!");
   console.log(guild_ids);
   // var Options = {
@@ -40,57 +44,48 @@ client.once("ready", () => {
   //   content: "Hello,world",
   // });
 });
+client.on("disconnect", () => {
+  botOnlin = false;
+  console.log("Bot disconnected");
+});
 const sendmessageInChat = function (text) {
+  // Initial attemp was to offload function so that i could access client and guild
   client.channels.cache.get(channelID).send(text);
 };
-client.on("messageCreate", (message) => {
-  if (message.content === prefix+"ping") {
-    message.channel.send("Pong!");
+const MemberFoundInChat = function (guildId, memberID) {
+  // while this worked for the first http request I meet a hard wall in the found member chat
+  const guild = client.guilds.cache.get(guildId);
+  let member = guild.members.cache.get(memberID);
+
+  if (!member) {
+    member = guild.members.cache.find(
+      (m) => m.user.username === memberNameOrID || m.nickname === memberNameOrID
+    );
   }
-  else if (message.content.includes('!nickname')) {
-    message.member.setNickname(message.content.replace(prefix+'nickname ', ''));
+};
+client.on("messageCreate", (message) => {
+  if (message.content === prefix + "ping") {
+    message.channel.send("Pong!");
+  } else if (message.content.includes("!nickname")) {
+    message.member.setNickname(
+      message.content.replace(prefix + "nickname ", "")
+    );
   }
 });
 
-
-// app.use(express.json()); // Middleware to parse JSON bodies
-
-// app.post("/send-message", (req, res) => {
-//   const { channelId, message } = req.body;
-
-//   const channel = client.channels.cache.get(channelId);
-//   if (channel) {
-//     channel
-//       .send(message)
-//       .then(() => res.status(200).json({ success: true }))
-//       .catch((error) =>
-//         res.status(500).json({ success: false, error: error.message })
-//       );
-//   } else {
-//     res.status(404).json({ success: false, error: "Channel not found" });
-//   }
-// });
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// app.post("/post-endpoint", (req, res) => {
-//   const { message } = req.body;
-
-//   // Send a message to a specific channel
-//   const channel = client.channels.cache.get(process.env.channelID);
-//   if (channel) {
-//     channel.send(message);
-//   }
-
-//   res.status(200).send("Message sent!");
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 client.login(process.env.TOKEN);
-module.exports = {
-  sendmessageInChat,
-};
+// (async () => {
+//   while (true) {
+//     if (botOnlin) {
+//       console.log("Bot is online!");
+//     } else {
+//       console.log("Bot is offline.");
+//     }
+//     await new Promise((resolve) => setTimeout(resolve, 5000));
+//   }
+// })();
+// module.exports = {
+//   sendmessageInChat,
+//   MemberFoundInChat,
+// };
+module.exports = { client };
--- src/server.js
+++ src/server.js
@@ -1,46 +1,108 @@
-const bot = require("./index");
+const { client } = require("./index");
+// const bot = require("./index");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { Guild } = require("discord.js");
+// Initializing an Express application
 const app = express();
 
 app.use(bodyParser.json()); // Middleware to parse JSON bodies
 app.use(cors());
+// Defining a POST endpoint to send a message to a Discord channel
 app.post("/send-message", async (req, res) => {
+  // Extracting channelId and message from the request body
   const { channelId, message } = req.body;
   // expects a POST request with a JSON body containing the channelId and message
-  // const channel = client.channels.cache.get(channelId);
   try {
-    // const channel = await client.channels.fetch(channelId);
-    // await channel.send(message);
+    // Fetching the channel by its ID and sending the message
+    const channel = await client.channels.fetch(channelId);
+    await channel.send(message);
+    // Responding with a success status
     // console.log(channelId);
     // console.log(message);
-    bot.sendmessageInChat(message);
+    // bot.sendmessageInChat(message);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    // Logging the error and responding with an error status
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.post("/findmember", async (req, res) => {
+  const { guildID, memberNameOrID } = req.body;
+  try {
+    // console.log(req.body);
+    // console.log(guildID);
+    // console.log(memberNameOrID);
+    // Fetching the guild by its ID
+    const guild = await client.guilds.fetch(guildID);
+    // Attempting to fetch the member by ID, then by username or nickname V
+    let member = await guild.members.fetch(memberNameOrID).catch(() => null);
+    if (!member) {
+      member = guild.members.cache.find(
+        (m) =>
+          m.user.username === memberNameOrID || m.nickname === memberNameOrID
+      );
+    }
+    // bot.MemberFoundInChat(guildID, memberNameOrID);
+
+    // bot.sendmessageInChat("Found ", member.user.tag);
+    // Responding with the member's tag if found, otherwise indicating failure
+    if (member) {
+      res.status(200).json({ success: true, member: member.user.tag });
+    } else {
+      res.status(404).json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+  // if (member) {
+  //   res.status(200).json({ member: member.user.tag });
+  //   bot.sendmessageInChat("Found ", member.user.tag);
+  // } else {
+  //   res.status(404).json({ member: null });
+  // }
+});
+
+app.post("/change-nickname", async (req, res) => {
+  const { guildId, memberId, newNickname } = req.body;
+
+  try {
+    const guild = await client.guilds.fetch(guildId);
+    const member = await guild.members.fetch(memberId);
+    await member.setNickname(newNickname);
     res.status(200).json({ success: true });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: error.message });
   }
 });
-app.post('/findmember', (req, res) => {
-  const { guildId, memberNameOrID } = req.body;
 
-  // Find the member in the guild
-  const guild = client.guilds.cache.get(guildId);
-  if (!guild) {
-      return res.status(404).json({ error: 'Guild not found' });
-  }
+app.post("/change-role", async (req, res) => {
+  const { guildId, memberId, roleId } = req.body;
 
-  const member = guild.members.cache.find(member => member.user.username === memberNameOrID || member.id === memberNameOrID);
-  if (member) {
-      res.json({ member: member.user.tag });
-      bot.sendmessageInChat("Found ",member.user.tag);
-  } else {
-      res.status(404).json({ error: 'Member not found' });
+  try {
+    const guild = await client.guilds.fetch(guildId);
+    const member = await guild.members.fetch(memberId);
+    const role = await guild.roles.fetch(roleId);
+
+    if (!role) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Role not found" });
+    }
+
+    await member.roles.add(role);
+    res.status(200).json({ success: true, message: "Role added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
-
+// Starting the Express server on the specified port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
